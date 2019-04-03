f64849de49b73cd1dac4baf8f906ed94e146711e
shyamraja <t7bhsh00@students.oamk.fi>
2019-04-03 13:02:55 +0300

    tried to manage next and previous buttons

---
+++ app/src/main/java/tapiopalonemi/fi/driversapp/Informationactivity.java
@@ -0,0 +1,76 @@
+package tapiopalonemi.fi.driversapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+//import android.widget.myListView;
+
+public class Informationactivity extends AppCompatActivity {
+
+    private ArrayList<String []> listOfLists;
+    private int index = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+        final String[] INFORMATIONS = new String[]{
+                "Adjust your mirrors correctly", "If the car don't start turn high beams on", "Use hand brake regularly",
+                "Don’t turn the wheels beforehand when making a left turn", "Maintain required distance always from other vehicles",
+                "Park with precision", "Be careful with signs on roads as well as traffic lights", "Always follow the speed limits",
+                "Give pedestrians the right-of-way in crosswalks", "Always check your seat belt"
+
+        };
+
+
+        final String[] INFORMATIONS2 = new String[]{
+                "Adjust your mirrors correctly", "If the car don't start turn high beams on", "Use hand brake regularly",
+                "Don’t turn the wheels beforehand when making a left turn", "Maintain required distance always from other vehicles",
+                "Park with precision", "Be careful with signs on roads as well as traffic lights", "Always follow the speed limits",
+                "Give pedestrians the right-of-way in crosswalks", "Always check your seat belt"
+
+        };
+        listOfLists = new ArrayList<>();
+        listOfLists.add(INFORMATIONS);
+        listOfLists.add(INFORMATIONS2);
+        nextInformation(null);
+        previousInformation (null);
+
+    }
+    private void showInformation(String [] information){
+
+        ListView myListView =  findViewById(R.id.information_list_view);
+        final ArrayAdapter<String> kk;
+        kk = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                information);
+
+
+        myListView.setAdapter(kk);
+    }
+
+    private void nextInformation(View view){
+        index = index + 1;
+        if (index > listOfLists.size())
+        {
+            index = listOfLists.size()-1;
+        }
+        showInformation(listOfLists.get(index));
+
+    }
+    private void previousInformation(View view) {
+        index = index - 1;
+        if (index < 0)
+        {
+            index = 0;
+        }
+            showInformation(listOfLists.get(index));
+
+    }
+
+    }
+
---
+++ app/src/main/java/tapiopalonemi/fi/driversapp/MainActivity.java
@@ -0,0 +1,166 @@
+package tapiopalonemi.fi.driversapp;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+//import android.graphics.Bitmap;
+//import android.graphics.Canvas;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private MyDBHandler db;
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+//                    navigation.setSelectedItemId(R.id.navigation_home);
+                    Log.i("HOME BOTTOM NAVIGATION", "HOME CLICKED");
+                    return true;
+                case R.id.navigation_exam:
+                    mTextMessage.setText(R.string.title_exam);
+                    Log.i("HOME BOTTOM NAVIGATION", "EXAM CLICKED");
+                    return true;
+                case R.id.navigation_results:
+                    mTextMessage.setText(R.string.title_results);
+                    Log.i("HOME BOTTOM NAVIGATION", "RESULTS CLICKED");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    public void toHome(MenuItem menuItem) {
+        Log.i("BOTTOM NAVIGATION", "to home navigation item clicked");
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void toExam(MenuItem menuItem) {
+        Log.i("BOTTOM NAVIGATION", "to exam navigation item clicked");
+        Intent intent = new Intent(getApplicationContext(), ExamActivity.class);
+        intent.putExtra("extra", "extra");
+        startActivity(intent);
+    }
+
+    public void toResults(MenuItem menuItem) {
+        Log.i("BOTTOM NAVIGATION", "to results navigation item clicked");
+        Intent intent = new Intent(getApplicationContext(), ResultsActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonToExam(View view) {
+        Log.i("BOTTOM NAVIGATION", "to exam button clicked");
+        Intent intent = new Intent(getApplicationContext(), ExamActivity.class);
+//        intent.putExtra("extra", "extra");
+        startActivity(intent);
+    }
+
+    public void buttonToResults(View view) {
+        Log.i("BOTTOM NAVIGATION", "to results button clicked");
+        Intent intent = new Intent(getApplicationContext(), ResultsActivity.class);
+//        intent.putExtra("extra", "extra");
+        startActivity(intent);
+    }
+    public void buttonToInformations(View view) {
+        Log.i("BOTTOM NAVIGATION", "to DRIVING TIPS button clicked");
+        Intent intent = new Intent(getApplicationContext(), Informationactivity.class);
+//        intent.putExtra("extra", "extra");
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = new MyDBHandler(this);
+//        db.onCreate(db.getWritableDatabase());
+//        db.getWritableDatabase();
+//        Log.i("DB", "going for questions");
+        if (!db.isDataLoaded()) {
+            Log.d("HOME", "data is not loaded");
+            db.firstRun(this);
+//            questions = db.loadQuestions();
+
+        }
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setSubtitle("Go Places>");
+        actionBar.setHomeButtonEnabled(true);
+
+
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//        navigation.setSelectedItemId(R.id.navigation_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.language, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.english:
+                Log.i("LANGUAGE SELECTION", "english");
+                setLocale("en");
+                return true;
+            case R.id.finnish:
+                Log.i("LANGUAGE SELECTION", "finnish");
+                setLocale("fi");
+                return true;
+            case R.id.swedish:
+                Log.i("LANGUAGE SELECTION", "swedish");
+                setLocale("sv");
+                return true;
+            case R.id.hungarian:
+                Log.i("LANGUAGE SELECTION", "hungarian");
+                setLocale("hu");
+                return true;
+            case R.id.nepali:
+                Log.i("LANGUAGE SELECTION", "nepali");
+                setLocale("ne");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+//        conf.locale = myLocale;
+        conf.setLocale(myLocale);
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, MainActivity.class);
+        startActivity(refresh);
+        finish();
+    }
+}
