3a158aab851ec8dff2dbd8247ca5d8665aa6cbfd
Shyamraja <t7bhsh00"students.oamk.fi>
2019-04-16 21:42:40 +0300

    Updated and fix next button

--- app/src/main/java/tapiopalonemi/fi/driversapp/Informationactivity.java
+++ app/src/main/java/tapiopalonemi/fi/driversapp/Informationactivity.java
@@ -2,18 +2,14 @@ package tapiopalonemi.fi.driversapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ViewFlipper;
 import java.util.ArrayList;
+import android.widget.Button;
 
-//import android.widget.myListView;
 
 public class Informationactivity extends AppCompatActivity {
-
-
 
 
     private ArrayList<String[]> listOfLists;
@@ -26,18 +22,15 @@ public class Informationactivity extends AppCompatActivity {
         final String[] INFORMATIONS = new String[]{
                 "Adjust your mirrors correctly", "If the car don't start turn high beams on", "Use hand brake regularly",
                 "Don’t turn the wheels beforehand when making a left turn", "Maintain required distance always from other vehicles",
-                "Park with precision", "Be careful with signs on roads as well as traffic lights", "Always follow the speed limits",
-                "Give pedestrians the right-of-way in crosswalks", "Always check your seat belt"
-
+                "Park with precision", "follow the vehicle of neighbouring lane"
         };
 
-        ListView myListView =  findViewById(R.id.information_list_view);
+        ListView myListView= findViewById(R.id.information_list_view);
 
         final String[] INFORMATIONS2 = new String[]{
-                "Adjust your mirrors correctly", "If the car don't start turn high beams on", "Use hand brake regularly",
-                "Don’t turn the wheels beforehand when making a left turn", "Maintain required distance always from other vehicles",
-                "Park with precision", "Be careful with signs on roads as well as traffic lights", "Always follow the speed limits",
-                "Give pedestrians the right-of-way in crosswalks", "Always check your seat belt"
+                "Be careful with signs on roads as well as traffic lights", "Always follow the speed limits",
+                "Give pedestrians the right-of-way in crosswalks", "Always check your seat belt","Park with precision",
+                "Keep winter Survival kit in winter time","plan route in advanced for long trips"
 
         };
         listOfLists = new ArrayList<>();
@@ -59,26 +52,43 @@ public class Informationactivity extends AppCompatActivity {
         myListView.setAdapter(kk);
     }
 
-    private void nextInformation(View view) {
+    public void nextInformation(View view) {
         index = index + 1;
-        if (index > listOfLists.size()) {
-            index = listOfLists.size() - 1;
-        }
-        Log.i("INFORMATIONS", "index: " + index);
-
+        setPreviousNextButtons(index);
         showInformation(listOfLists.get(index));
 
     }
 
-    private void previousInformation(View view) {
+    public void previousInformation(View view) {
         index = index - 1;
-        if (index < 0) {
-            index = 0;
-        }
-        Log.i("INFORMATIONS2", "index: " + index);
+        setPreviousNextButtons(index);
         showInformation(listOfLists.get(index));
+
+    }
+
+    private void setPreviousNextButtons(int idx) {
+        Button previousButton = findViewById(R.id.button2);
+        Button nextButton = findViewById(R.id.button3);
+
+        if (idx <= 0) {
+            index = 0;
+            previousButton.setEnabled(false);
+            nextButton.setEnabled(true);
+        } else {
+            previousButton.setEnabled(true);
+        }
+        if (index >= listOfLists.size()) {
+            index = listOfLists.size() - 1;
+            nextButton.setEnabled(false);
+
+        } else {
+            if (index == listOfLists.size() - 1) {
+                nextButton.setEnabled(false);
+            } else {
+                nextButton.setEnabled(true);
+            }
+        }
 
     }
 
 }
-
