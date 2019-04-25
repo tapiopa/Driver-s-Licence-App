4a338c83d43bc2fc746b18ae329223824a85c67e
Tapio Paloniemi <t7pari00@students.oamk.fi>
2019-04-25 10:42:22 +0300

    Work on DrivingSchools

--- app/src/main/java/tapiopalonemi/fi/driversapp/AdAdapter.java
+++ app/src/main/java/tapiopalonemi/fi/driversapp/AdAdapter.java
@@ -1,12 +1,18 @@
 package tapiopalonemi.fi.driversapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 //import android.util.Log;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.jetbrains.annotations.NotNull;
@@ -15,30 +21,58 @@ import java.util.ArrayList;
 
 class AdAdapter extends ArrayAdapter<AdItem> {
 
-    public AdAdapter(Context context, ArrayList<AdItem> choices) {
-        super(context, 0, choices);
+    public AdAdapter(Context context, ArrayList<AdItem> ads) {
+        super(context, 0, ads);
     }
 
     @NotNull
     @Override
-    public View getView(int position, View convertView, @NotNull ViewGroup parent) {
-        AdItem ad = getItem(position);
+    public View getView(int position, View convertView, @NotNull final ViewGroup parent) {
+        final AdItem ad = getItem(position);
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_choice, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_drivingschools, parent, false);
         }
-        TextView choiceQuestion = convertView.findViewById(R.id.choice_question);
-        TextView choiceAnswer = convertView.findViewById(R.id.choice_answer);
-        TextView choiceIsRight = convertView.findViewById(R.id.choice_answer_is_right);
+        ImageView image = convertView.findViewById(R.id.image);
 
-//        TextView userAnswer = parent.findViewById(R.id.user_answer);
-        assert ad != null;
-        choiceQuestion.setTextSize(12.0f);
-        choiceQuestion.setPadding(12, 12, 0, 0);
+        TextView name = convertView.findViewById(R.id.name);
+        final EditText editText = convertView.findViewById(R.id.editText);
 
-        choiceAnswer.setTextSize(12.0f);
-        choiceAnswer.setPadding(0, 0, 0, 12);
-        choiceIsRight.setTextSize(12.0f);
-        choiceIsRight.setPadding(12, 0, 0, 12);
+        Button map = convertView.findViewById(R.id.map);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("OPEN MAP", "MAP address: " + ad.getMap() +  ", onClick: OPEN MAP");
+                // Map point based on address
+                Uri location = Uri.parse("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California");
+                // Or map point based on latitude/longitude
+                // Uri location = Uri.parse("geo:37.422219,-122.08364?z=14"); // z param is zoom level
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+                parent.getContext().startActivity(mapIntent);
+            }
+        });
+
+        Button call = convertView.findViewById(R.id.call);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("CALL", "onClick: MAKE A CALL, phone: " + ad.getCall());
+                Uri number = Uri.parse("tel:4567123");
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+                parent.getContext().startActivity(callIntent);
+            }
+        });
+
+        Button go = convertView.findViewById(R.id.go);
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                Log.d("text", "GO, web page address: " + ad.getWebPage() + ", onClick: Go " + text);
+                Uri webpage = Uri.parse(text);
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                parent.getContext().startActivity(webIntent);
+            }
+        });
 
         return convertView;
     }
--- app/src/main/java/tapiopalonemi/fi/driversapp/AdItem.java
+++ app/src/main/java/tapiopalonemi/fi/driversapp/AdItem.java
@@ -1,6 +1,57 @@
 package tapiopalonemi.fi.driversapp;
 
 public class AdItem {
+    private String name;
+    private String map;
+    private String webPage;
+    private String call;
+    private int image;
 
+    public AdItem(String name, String map, String webPage, String call, int image) {
+        this.name = name;
+        this.map = map;
+        this.webPage = webPage;
+        this.call = call;
+        this.image = image;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMap() {
+        return map;
+    }
+
+    public void setMap(String map) {
+        this.map = map;
+    }
+
+    public String getWebPage() {
+        return webPage;
+    }
+
+    public void setWebPage(String webPage) {
+        this.webPage = webPage;
+    }
+
+    public String getCall() {
+        return call;
+    }
+
+    public void setCall(String call) {
+        this.call = call;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
 }
